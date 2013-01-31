f9524f5c1e2b0ad5e91f300842be96af23e35a54
Tim de Koning <github@king-management.nl>
2013-01-31 16:23:16 +0100

    Added Sydney-region as requested in issue #8

--- app/view/SettingsPanel.js
+++ app/view/SettingsPanel.js
@@ -115,6 +115,7 @@ Ext.define('SdbNavigator.view.SettingsPanel', {
 						['sdb.us-west-2.amazonaws.com', 'US-West (Oregon)'],
 						['sdb.eu-west-1.amazonaws.com', 'EU (Ireland)'],
 						['sdb.ap-southeast-1.amazonaws.com', 'Asia Pacific (Singapore)'],
+						['sdb.ap-southeast-2.amazonaws.com', 'Asia Pacific (Sydney)'],
 						['sdb.ap-northeast-1.amazonaws.com', 'Asia Pacific (Tokyo)'],
 						['sdb.sa-east-1.amazonaws.com', 'South America (Sao Paulo)']
 					],
